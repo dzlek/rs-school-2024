ae460c33bbfc5d1a48a8101e4050b7e5977486da
dzlek <6894255@gmail.com>
2024-11-24 22:42:14 +0300

    feat: add js

--- christmas-shop/src/js/main.js
+++ christmas-shop/src/js/main.js
@@ -1,1 +1,22 @@
-console.log("TEST");
+console.log("Loaded...");
+
+document.addEventListener("DOMContentLoaded", () => {
+  const body = document.querySelector("body");
+  const btnBurger = document.querySelector(".menu-btn-burger");
+  const menu = document.querySelector(".menu");
+  const navItem = menu.querySelectorAll("a");
+
+  btnBurger.addEventListener("click", () => {
+    btnBurger.classList.toggle("active");
+    menu.classList.toggle("active");
+    body.classList.toggle("no-scroll");
+  });
+
+  navItem.forEach((item) => {
+    item.addEventListener("click", () => {
+      btnBurger.classList.remove("active");
+      menu.classList.remove("active");
+      body.classList.remove("no-scroll");
+    });
+  });
+});
