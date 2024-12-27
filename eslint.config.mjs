66c4b31d65c33dc781c91306e27728de3de86d12
dzlek <6894255@gmail.com>
2024-12-27 13:57:38 +0300

    init: start hangman-task

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,23 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-
+import pluginPrettier from "eslint-plugin-prettier";
+import prettierConfig from "eslint-config-prettier";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {languageOptions: { globals: globals.browser }},
+  {
+    languageOptions: {
+      globals: globals.browser,
+    },
+  },
   pluginJs.configs.recommended,
+  {
+    plugins: {
+      prettier: pluginPrettier,
+    },
+    rules: {
+      ...prettierConfig.rules,
+      "prettier/prettier": "error",
+    },
+  },
 ];
